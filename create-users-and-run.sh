f2b2cecf6de780af235116894a02a1aafacf202a
freiheitsnetz <raphael.github@elsner-online.com>
2019-02-26 11:23:35 +0100

    fixed GITHUB_USERS specification

--- create-users-and-run.sh
+++ create-users-and-run.sh
@@ -8,24 +8,24 @@ if [ -n "$USERNAME" ]; then
     adduser -G rsync -s /bin/sh -D "$USERNAME"
     echo "$USERNAME":$(head -c30 /dev/urandom | base64) | chpasswd
     mkdir /home/"$USERNAME"/.ssh
-    for GITHUB_USER in $GITHUB_USERS; do
-        wget -q -O /home/"$USERNAME"/.ssh/authorized_keys https://github.com/"$GITHUB_USER".keys
-    done
+    if [ ! -z "${GITHUB_USERS:-}" ]; then
+        for GITHUB_USER in $GITHUB_USERS; do
+            wget -q -O /home/"$USERNAME"/.ssh/authorized_keys https://github.com/"$GITHUB_USER".keys
+        done
+    fi
     echo "${SSH_KEY:-}" >> /home/"$USERNAME"/.ssh/authorized_keys
     chown -R "$USERNAME":rsync /home/"$USERNAME"/.ssh
     chmod -R go-wx /home/"$USERNAME"/.ssh
 else
-    if [ ! -z "${GITHUB_USERS:-}" ]; then
-        for GITHUB_USER in $GITHUB_USERS; do
-            USERNAME="$GITHUB_USER"
-            adduser -G rsync -s /bin/sh -D "$USERNAME"
-            echo "$USERNAME":$(head -c30 /dev/urandom | base64) | chpasswd
-            mkdir /home/"$USERNAME"/.ssh
-            wget -q -O /home/"$USERNAME"/.ssh/authorized_keys https://github.com/"$GITHUB_USER".keys
-            chown -R "$USERNAME":rsync /home/"$USERNAME"/.ssh
-            chmod -R go-wx /home/"$USERNAME"/.ssh
-        done
-    fi
+    for GITHUB_USER in $GITHUB_USERS; do
+        USERNAME="$GITHUB_USER"
+        adduser -G rsync -s /bin/sh -D "$USERNAME"
+        echo "$USERNAME":$(head -c30 /dev/urandom | base64) | chpasswd
+        mkdir /home/"$USERNAME"/.ssh
+        wget -q -O /home/"$USERNAME"/.ssh/authorized_keys https://github.com/"$GITHUB_USER".keys
+        chown -R "$USERNAME":rsync /home/"$USERNAME"/.ssh
+        chmod -R go-wx /home/"$USERNAME"/.ssh
+    done
 fi
 
 exec /usr/sbin/sshd -eD
