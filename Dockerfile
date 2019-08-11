81ffbb0dd73f057e9a66a5dcbaa857b99c71eed6
Patrick Neumann <patrick@neuma-nn.de>
2019-08-11 16:15:06 +0200

    sshd_config now part of image (sed-generated), keys in subdirectory

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,16 @@ RUN apk add -U \
     rsync && \
     rm -f /var/cache/apk/*
 
-VOLUME ["/etc/ssh/"]
-ADD sshd_config /etc/ssh/
+RUN sed -i 's/[#\s]*PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config && \
+    sed -i 's/[#\s]*PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config && \
+    sed -ri 's/[#\s]*HostKey \/etc\/ssh\/(.+)/HostKey \/etc\/ssh\/keys\/\1/' /etc/ssh/sshd_config
+
+#RUN grep HostKey /etc/ssh/sshd_config && \
+#    grep PermitRootLogin /etc/ssh/sshd_config && \
+#    grep PasswordAuthentication /etc/ssh/sshd_config
+
+VOLUME ["/etc/ssh/keys/"]
 ADD sshd.sh /usr/local/bin/
 
 EXPOSE 22
 ENTRYPOINT ["sshd.sh"]
-#STOPSIGNAL INT
--- sshd.sh
+++ sshd.sh
@@ -2,10 +2,12 @@
 
 set -eu
 
+SSH_KEY_DIR=/etc/ssh/keys
+
 # Initialize separate volume /etc/ssh: Create sshd host keys
-[ -f /etc/ssh/ssh_host_rsa_key ] || /usr/bin/ssh-keygen -q -t rsa -f /etc/ssh/ssh_host_rsa_key -C '' -N ''
-[ -f /etc/ssh/ssh_host_ecdsa_key ] || /usr/bin/ssh-keygen -q -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -C '' -N ''
-[ -f /etc/ssh/ssh_host_ed25519_key ] || /usr/bin/ssh-keygen -q -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -C '' -N ''
+[ -f $SSH_KEY_DIR/ssh_host_rsa_key ] || /usr/bin/ssh-keygen -q -t rsa -f $SSH_KEY_DIR/ssh_host_rsa_key -C '' -N ''
+[ -f $SSH_KEY_DIR/ssh_host_ecdsa_key ] || /usr/bin/ssh-keygen -q -t ecdsa -f $SSH_KEY_DIR/ssh_host_ecdsa_key -C '' -N ''
+[ -f $SSH_KEY_DIR/ssh_host_ed25519_key ] || /usr/bin/ssh-keygen -q -t ed25519 -f $SSH_KEY_DIR/ssh_host_ed25519_key -C '' -N ''
 
 # Initialize container volume on first run
 if [ ! -f ~/.is_initialized ]; then
